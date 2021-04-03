3b0afdee95cbfad1cc09f85a952796af8018320a
barisgul <bariss.gull@gmail.com>
2021-04-04 01:07:08 +0300

    Driverfactory added

---
+++ src/Framework.Core/Entites/BrowserType.cs
@@ -0,0 +1,10 @@
+﻿namespace Framework.Core.Entites
+{
+    public enum BrowserType
+    {
+        NONE=0,
+        CHROME=1,
+        IE=2,
+        FIREFOX=3
+    }
+}
---
+++ src/Framework.Core/Infrastructure/Managers/DriverFactory.cs
@@ -0,0 +1,38 @@
+﻿using Framework.Core.Entites;
+using OpenQA.Selenium;
+using System;
+
+namespace Framework.Core.Infrastructure.Managers
+{
+    public class DriverFactory : IDisposable
+    {
+        private IWebDriver driver;
+        public DriverFactory()
+        {
+            driver = null;
+        }
+
+        public IWebDriver GetDriver(BrowserType browserType)
+        {
+            if (browserType.Equals(BrowserType.CHROME))
+            {
+                throw new NotImplementedException();
+                //return driver
+            }
+
+            if (browserType.Equals(BrowserType.FIREFOX))
+            {
+                throw new NotImplementedException();
+                //return driver
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
