5ccd39b749ec3d676e58a1adc0a96c6d0572c4ec
barisgul <bariss.gull@gmail.com>
2021-04-04 21:10:52 +0300

    folder renamed

---
+++ test/UnitTest/Framework.ApiHandler.UnitTest/DriverFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using Framework.Core.Infrastructure.Entites;
+using Framework.Core.Infrastructure.Managers;
+using Moq;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Framework.ApiHandler.UnitTest
+{
+    public class DriverFactoryTests
+    {
+        Mock<IWebDriver> mockDriver;
+        public DriverFactoryTests()
+        {
+            mockDriver = new Mock<IWebDriver>();
+        }
+
+        [Fact]
+        public void test()
+        {
+            DriverFactory driverFactory = new DriverFactory();
+            var tmp = driverFactory.GetDriver(ExecutionEnvironment.LOCAL, Core.Entites.BrowserType.CHROME);
+        }
+    }
+}
