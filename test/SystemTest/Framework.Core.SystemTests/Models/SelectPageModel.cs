0b1d75c5d5d5bd4dce446aba06f67eb54fec7274
barisgul <bariss.gull@gmail.com>
2021-04-06 17:19:34 +0300

    SelectPage system tests added

---
+++ test/SystemTest/Framework.Core.SystemTests/Models/SelectPageModel.cs
@@ -0,0 +1,46 @@
+﻿using Framework.Core.Domain;
+using OpenQA.Selenium;
+using System.IO;
+using System.Reflection;
+
+namespace Framework.Core.SystemTests.Models
+{
+    public class SelectPageModel : BasePage
+    {
+        public IWebElement SelectTwo => driver.FindElement(By.XPath("//*[@id='selectWithoutMultiple']/option[2]"));
+        public IWebElement LinkRoquefort => driver.FindElement(By.XPath("//*[@id='selectWithMultipleEqualsMultiple']/option[text() = 'Roquefort']"));
+        public IWebElement RandomMultipleOne => driver.FindElement(By.XPath("//*[@id='selectWithRandomMultipleValue']/option[text() = 'one']"));
+
+        public void GoToInternalPage()
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) +
+                                                                           "\\Sources\\selectPage.html";
+            NavigateTo(path);
+        }
+
+        public void ClickSelectTwo()
+        {
+            SelectTwo.Click();
+        }
+
+        public void ClickLinkLinkRoquefort()
+        {
+            LinkRoquefort.Click();
+        }
+
+        public void ClickRandomMultipleOne()
+        {
+            RandomMultipleOne.Click();
+        }
+
+        public string GetPageTitle()
+        {
+            return driver.Title;
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+    }
+}
---
+++ test/SystemTest/Framework.Core.SystemTests/Tests/SelectPageTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Framework.Common;
+using Framework.Core.SystemTests.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Framework.Core.SystemTests.Tests
+{
+    public class SelectPageTests : IDisposable
+    {
+        SelectPageModel selectPageModel; 
+        public SelectPageTests()
+        {
+            selectPageModel = new SelectPageModel();
+            selectPageModel.GoToInternalPage();
+        }
+
+        [Fact]
+        [Trait("Category", "WebDriver.SystemTest")]
+        public void ClickSelectTwo()
+        {
+            selectPageModel.ClickSelectTwo();
+        }
+
+        [Fact]
+        [Trait("Category", "WebDriver.SystemTest")]
+        public void ClickLinkLinkRoquefort()
+        {
+            selectPageModel.ClickLinkLinkRoquefort();
+        }
+
+        [Fact]
+        [Trait("Category", "WebDriver.SystemTest")]
+        public void ClickRandomMultipleOne()
+        {
+            selectPageModel.ClickLinkLinkRoquefort();
+        }
+
+        [Fact]
+        [Trait("Category", "WebDriver.SystemTest")]
+        public void Click_CanClickOnButtonAndFollowIt()
+        {
+            Logger.Info("Click_CanClickOnALinkAndFollowIt test started");
+            selectPageModel.ClickSelectTwo();
+
+            string pageTitle = selectPageModel.GetPageTitle();
+
+            pageTitle.Should().Contain("Multiple Selection");
+        }
+
+
+        public void Dispose()
+        {
+            selectPageModel.Dispose();
+        }
+    }
+}
